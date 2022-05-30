b8f8ddb15382e7fc58bee0fafb8d325d280baecb
Kite <ixkaito@gmail.com>
2022-05-30 12:19:19 +0900

    Improve README and seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,10 +2,11 @@
  * @type {import('next-seo/lib/types').NextSeoProps}
  **/
 const config = {
+  titleTemplate: '%s | Intersection Observer Performance Test',
   openGraph: {
     type: 'article',
     locale: 'en_US',
-    site_name: 'NextSSS',
+    site_name: 'Intersection Observer Performance Test',
   },
   twitter: {
     cardType: 'summary_large_image',
