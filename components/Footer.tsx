2c84576205c548c2b69f3816f3de981be49359ff
Kite <ixkaito@gmail.com>
2022-05-30 11:34:52 +0900

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,29 @@
+import Link from 'next/link'
+
 const Footer: React.FC = () => {
   return (
     <footer className="p-4 bg-gray-50 border-t border-gray-200">
-      <p className="text-center">© 2021 KITERETZ inc.</p>
+      <p className="text-center">
+        © 2022{' '}
+        <Link
+          className="underline hover:no-underline"
+          href="https://twitter.com/ixkaito"
+          target="_blank"
+          rel="noopener"
+        >
+          Kite
+        </Link>{' '}
+        /{' '}
+        <Link
+          className="underline hover:no-underline"
+          href="https://kiteretz.com/about/"
+          target="_blank"
+          rel="noopener"
+        >
+          KITERETZ inc
+        </Link>
+        .
+      </p>
     </footer>
   )
 }
